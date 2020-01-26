3adc683bb0c92fefcc5ce33f141e4d12a9e41579
KrissDrawing <krx95@o2.pl>
2020-01-26 15:19:12 +0100

    Add Brightness slider

--- app/src/main/java/com/kriss/roomcontrol/MainActivity.kt
+++ app/src/main/java/com/kriss/roomcontrol/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
 import com.google.firebase.database.FirebaseDatabase
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,6 +15,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var turnOnLightsButton : Button
+    lateinit var changeBrightnessSeekBar: SeekBar
+    lateinit var testText: TextView
     var switcher: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,18 +24,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         turnOnLightsButton = findViewById(R.id.turnOnLightsButton)
+        changeBrightnessSeekBar = findViewById(R.id.changeBrightnessSeekBar)
+        testText = findViewById(R.id.testText)
 
-        turnOnLightsButton.setOnClickListener {
-            saveData();
+        changeBrightnessSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                testText.text = changeBrightnessSeekBar.progress.toString()
+                AdjustBrightness()
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+
+            }
         }
 
+        )
 
-
+        turnOnLightsButton.setOnClickListener {
+            saveData()
+        }
     }
+    private fun AdjustBrightness(){
+        val ref = FirebaseDatabase.getInstance().getReference("LED_CONTROL")
+        ref.child("BRIGHTNESS").setValue(changeBrightnessSeekBar.progress.toString())
+    }
+
     private fun saveData(){
-        val ref = FirebaseDatabase.getInstance().getReference("switcher")
-        ref.child("Name").setValue(switcher)
+        val ref = FirebaseDatabase.getInstance().getReference("LED_CONTROL")
+        ref.child("TURN").setValue(switcher.toString())
+
         switcher = !switcher
     }
 }
